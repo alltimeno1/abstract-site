8955282e3e23375fa623e0099d847579166eddfc
Sungjun Kim <hzcd108@gmail.com>
2022-06-03 15:09:53 +0900

    클래스 작성

--- src/index.js
+++ src/index.js
@@ -1,10 +1,89 @@
-class Site {}
+class Site {
+    constructor() {
+        this.boards = [];
+    }
 
-class Board {}
+    addBoard(board) {
+        if (this.boards.find((e) => e.name === board.name)) {
+            throw new Error();
+        }
 
-class Article {}
+        this.boards.push(board);
+        board.added = this;
+    }
 
-class Comment {}
+    findBoardByName(keyword) {
+        return this.boards.find((e) => e.name === keyword);
+    }
+}
+
+class Board {
+    constructor(name) {
+        if (!name) {
+            throw new Error();
+        }
+
+        this.name = name;
+        this.added = null;
+        this.articles = [];
+    }
+
+    publish(article) {
+        if (!this.added) {
+            throw new Error();
+        }
+
+        this.articles.push(article);
+        article.id = `${this.name}-${this.articles.length + 1}`;
+        article.added = this;
+    }
+
+    getAllArticles() {
+        return this.articles;
+    }
+}
+
+class Article {
+    constructor({ subject, content, author }) {
+        this.id = null;
+        this.createdDate = new Date().toISOString();
+
+        if (!subject || !content || !author) {
+            throw new Error();
+        }
+
+        this.subject = subject;
+        this.content = content;
+        this.author = author;
+        this.added = null;
+        this.comments = [];
+    }
+
+    reply(comment) {
+        if (!this.added) {
+            throw new Error();
+        }
+
+        this.comments.push(comment);
+    }
+
+    getAllComments() {
+        return this.comments;
+    }
+}
+
+class Comment {
+    constructor({ content, author }) {
+        this.createdDate = new Date().toISOString();
+
+        if (!content || !author) {
+            throw new Error();
+        }
+
+        this.content = content;
+        this.author = author;
+    }
+}
 
 module.exports = {
     Site,
